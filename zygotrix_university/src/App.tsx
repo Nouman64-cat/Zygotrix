9b4099f64d639dda50705d34a4c30679945004be
Nouman Ejaz <noumanejazghost@gmail.com>
2025-11-01 00:35:58 +0500

    unused pages are removed from the university

--- zygotrix_university/src/App.tsx
+++ zygotrix_university/src/App.tsx
@@ -3,19 +3,6 @@ import { Toaster } from "react-hot-toast";
 import MainLayout from "./layouts/MainLayout";
 import DashboardLayout from "./layouts/DashboardLayout";
 import HomePage from "./pages/HomePage";
-import CoursesPage from "./pages/CoursesPage";
-import CourseDetailPage from "./pages/CourseDetailPage";
-import PathsPage from "./pages/PathsPage";
-import PracticePage from "./pages/PracticePage";
-import CommunityPage from "./pages/CommunityPage";
-import ResourcesPage from "./pages/ResourcesPage";
-import SimulationStudioPage from "./pages/SimulationStudioPage";
-import AboutPage from "./pages/AboutPage";
-import SupportPage from "./pages/SupportPage";
-import EnterprisePage from "./pages/EnterprisePage";
-import ContactPage from "./pages/ContactPage";
-import AccessibilityPage from "./pages/AccessibilityPage";
-import NewsletterPage from "./pages/NewsletterPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import OverviewDashboardPage from "./pages/dashboard/OverviewDashboardPage";
 import CoursesDashboardPage from "./pages/dashboard/CoursesDashboardPage";
@@ -36,19 +23,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="courses" element={<CoursesPage />} />
-          <Route path="courses/:slug" element={<CourseDetailPage />} />
-          <Route path="paths" element={<PathsPage />} />
-          <Route path="practice" element={<PracticePage />} />
-          <Route path="community" element={<CommunityPage />} />
-          <Route path="resources" element={<ResourcesPage />} />
-          <Route path="simulation-studio" element={<SimulationStudioPage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="support" element={<SupportPage />} />
-          <Route path="enterprise" element={<EnterprisePage />} />
-          <Route path="contact" element={<ContactPage />} />
-          <Route path="accessibility" element={<AccessibilityPage />} />
-          <Route path="newsletter" element={<NewsletterPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
         <Route path="signin" element={<SignInPage />} />

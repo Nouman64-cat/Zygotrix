42747f602adb8bd88df3d1ca83dd37e5c5cf772e
Nouman Ejaz <noumanejazghost@gmail.com>
2026-01-01 15:18:43 +0500

    font removed and now well be used form the cdn

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class', '.dark'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Axiforma', 'sans-serif'],
+      },
+    },
+  },
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",

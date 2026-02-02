53f28e68882290e59a48e328ab45398d3fb1dce3
Nouman Ejaz <noumanejazghost@gmail.com>
2026-02-02 22:26:46 +0500

    unused files removed and font included for the marketing site

--- backend/install_resend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install resend package for email functionality
-
-echo "Installing resend package..."
-pip install resend==2.7.0
-
-echo "Resend package installed successfully!"
-echo "Please restart the uvicorn server for changes to take effect."
